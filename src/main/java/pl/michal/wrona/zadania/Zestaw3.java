dc8d9140ad4883b2fa833e997fd8ef877e7e885f
Michal Wrona <wrona.michal4444@gmail.com>
2022-07-20 15:27:49 +0200

    Initial Commit6

--- src/main/java/pl/michal/wrona/zadania/Zestaw3.java
+++ src/main/java/pl/michal/wrona/zadania/Zestaw3.java
@@ -17,26 +17,79 @@ public class Zestaw3 {
         System.out.println(array);
     }
 
-    public static void zadanie2(String word){
+    public static void zadanie2(String word) {
         int length = word.length();
         StringBuilder sb = new StringBuilder();
-        int count=0;
+        int count = 0;
 
-        for(int i =0; i<length; i++){
-            if(i==length-1){
+        for (int i = 0; i < length; i++) {
+            if (i == length - 1) {
                 sb.append(word.charAt(i)).append(count);
                 break;
             }
-            if(word.charAt(i)==word.charAt(i+1)){
+            if (word.charAt(i) == word.charAt(i + 1)) {
                 count++;
-            }
-            else{
+            } else {
                 sb.append(word.charAt(i)).append(count);
-                count=1;
+                count = 1;
             }
         }
 
         System.out.println(sb);
 
+    }
+
+    public static void zadanie2a(String word) {
+        int length = word.length();
+        StringBuilder sb = new StringBuilder();
+        int count = 0;
+
+        for (int i = 0; i < length; i++) {
+            if (i == length - 1) {
+                sb.append(word.charAt(i)).append(count);
+                break;
+            }
+            if (word.charAt(i) == word.charAt(i + 1)) {
+                count++;
+            } else {
+                sb.append(word.charAt(i)).append(count);
+                count = 1;
+            }
+        }
+
+        System.out.println(sb);
+
+    }
+
+    public static void zadanie3(String word) {
+        int length = word.length();
+        StringBuilder sb = new StringBuilder();
+        int count = 0;
+        for (int i = 0; i < length; i++) {
+            switch (word.charAt(i)) {
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                case '0':
+                    count++;
+            }
+
+            if (count == 1) {
+                sb.append(word.charAt(i - 1));
+                int number = word.charAt(i);
+            }
+            if (count > 1) {
+                sb.append(word.charAt(i - count+1));
+
+            }
+
+
+        }
     }
 }
